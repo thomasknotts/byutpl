40db6ff142c29dd332e6f59e62e17c911994686f
Thomas A Knotts IV <79026449+thomasknotts@users.noreply.github.com>
2021-05-16 00:18:33 -0600

    Update srk.py
    Started writing docstrings and documentation.

--- eos/srk.py
+++ eos/srk.py
@@ -1,14 +1,14 @@
 # Copyright (C) 2019 Thomas Allen Knotts IV - All Rights Reserved          #
-# This file, srkprops.py, is a python library that calculates              #
+# This file, srk.py, is a python library that calculates                   #
 # thermodynamic properties such as V, P, H, S, U, and G from the Soave,    #
 # Redlich, Kwong equation of state.                                        #
 #                                                                          #
-# srkprops.py is free software: you can redistribute it and/or             #
+# srk.py is free software: you can redistribute it and/or                  #
 # modify it under the terms of the GNU General Public License as           #
 # published by the Free Software Foundation, either version 3 of the       #
 # License, or (at your option) any later version.                          #
 #                                                                          #
-# srkprops.py is distributed in the hope that it will be useful,           #
+# srk.py is distributed in the hope that it will be useful,                #
 # but WITHOUT ANY WARRANTY; without even the implied warranty of           #
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
 # GNU General Public License for more details.                             #
@@ -19,7 +19,7 @@
 #                                                                          #
 
 # ======================================================================== #
-# srkprops.py                                                              #
+# srk.py                                                                   #
 #                                                                          #
 # Thomas A. Knotts IV                                                      #
 # Brigham Young University                                                 #
@@ -28,80 +28,169 @@
 # Email: thomas.knotts@byu.edu                                             #
 # ======================================================================== #
 # Version 1.0 - October 2019                                               #
+# Version 2.0 - May 2021 Changed name form srkprops to srk                 #
 # ======================================================================== #
+"""
+This module contains functions that calculate thermodynamic properties  
+at a given T and P using the Soave-Redlich-Kwong equation of state.     
+Functions are also available that calculate various partial derivatives of  
+P as a function of T and v. The functions require the critical             
+temperature, pressure, and acentric factor of the compound of interest.  
+                                                                         
+The equations follow that presented in Chapter 4 of The Properties of    
+Gases and Liquids, 5th ed. by Poling, Prausnitz, and O'Connell.  The      
+volume is solved by placing the equations on state in the dimensionless  
+cubic form.  For more information, see [1].
 
-# ======================================================================== #
-# srkprops.py                                                              #
-#                                                                          #
-# This library contains functions that calculate thermodynamic properties  #
-# at a given T and P using the Soave-Redlich-Kwong equation off state.     #
-# Functions are also available that calculate various partial derivatives  #
-# as a function of T and v. The functions require the critical             #
-# temperature, pressure, and acentric factor of the compound of interest.  #
-#                                                                          #
-# The equations follow that presented in Chapter 4 of The Properties of    #
-# Gases and Liquids, 5th ed. by Poling, Prausnitz, and O'Connel.  The      #
-# volume is solved by placing the equations on state in the dimensionless  #
-# cubic form.  For more information, see the reference above.              #
-#                                                                          #
-# The library can be loaded into python via the following command:         #
-# import srkprops as srk                                                   #
-#                                                                          #
-# ----------------------------------------------------------------------   #
-# DEFINITION OF INPUT PARAMETERS FOR FUNCTIONS                             #
-# ----------------------------------------------------------------------   #
-# Symbol             Property                               Units          #
-# ----------------------------------------------------------------------   #
-# t                  system temperature                     K              #
-# p                  system pressure                        Pa             #
-# v                  system molar volume                    m**3/mol       #
-# tc                 critical temperature of compound       K              #
-# pc                 critical pressure of compound          Pa             #
-# w                  acentric factor of compound            unitless       #
-# ----------------------------------------------------------------------   #
-#                                                                          #
-#                                                                          #
-# ----------------------------------------------------------------------   #
-# AVAILABLE PROPERTY FUNCTIONS                                             # 
-# ----------------------------------------------------------------------   #
-# Function           Return Value                           Units          #
-# ----------------------------------------------------------------------   #
-# vl(t,p,tc,pc,w)    liquid molar volume, srk eos           m**3/mol       #
-# vv(t,p,tc,pc,w)    vapor molar volume, srk eos            m**3/mol       #
-# zl(t,p,tc,pc,w)    liquid compressibility, srk eos        unitless       #
-# zv(t,p,tc,pc,w)    vapor compressibility, srk eos         unitless       #
-#                                                                          #
-# The functions listed above have been validated against the data in       #
-# the tables on pages 6.11 and 6.12 of the 5th edition of The Properties   #
-# of Gases and Liquids.                                                    #
-#                                                                          #
-# The residual functions (found below) are in development and have not     #
-# been validated as of 10/1/2019.                                          #
-# ======================================================================== #
+This module is part of the dippr package.              
 
+Import the module using
+
+  import dippr.eos.srk  
+
+Imported in the manner, the functions can be called as  
+  
+  srk.vl(t,p,tc,pc,w)
+
+which would return the liquid volume at t and p for the compound described
+by tc, pc, and w.
+
+Many functions are available, but several as auxillary function that 
+are used in the main functions. Below, the lists of input parameters
+is found followed by the list of functions that are most useful.   
+
+----------------------------------------------------------------------
+DEFINITION OF INPUT PARAMETERS FOR FUNCTIONS                          
+----------------------------------------------------------------------
+Symbol              Property                                Units       
+----------------------------------------------------------------------
+t                   system temperature                      K           
+p                   system pressure                         Pa          
+v                   system molar volume                     m**3/mol    
+tc                  critical temperature of compound        K           
+pc                  critical pressure of compound           Pa          
+w                   acentric factor of compound             unitless    
+----------------------------------------------------------------------
+                                                                      
+                                                                      
+----------------------------------------------------------------------
+PROPERTY FUNCTIONS                                          
+----------------------------------------------------------------------
+Function            Return Value                            Units       
+----------------------------------------------------------------------
+vl(t,p,tc,pc,w)     liquid molar volume                     m**3/mol    
+vv(t,p,tc,pc,w)     vapor molar volume                      m**3/mol    
+zl(t,p,tc,pc,w)     liquid compressibility                  unitless    
+zv(t,p,tc,pc,w)     vapor compressibility                   unitless
+hrl(t,p,tc,pc,w)    liquid residual enthalpy                J/mol 
+hrv(t,p,tc,pc,w)    vapor residual enthalpy                 J/mol   
+srl(t,p,tc,pc,w)    liquid residual entropy                 J/mol/K 
+srv(t,p,tc,pc,w)    vapor residual entropy                  J/mol/K 
+arl(t,p,tc,pc,w)    liquid residual Helmholtz energy        J/mol 
+arv(t,p,tc,pc,w)    vapor residual Helmholtz energy         J/mol 
+grl(t,p,tc,pc,w)    liquid residual Gibbs energy            J/mol 
+grv(t,p,tc,pc,w)    vapor residual Gibbs energy             J/mol 
+lnphil(t,p,tc,pc,w) natural log liquid fugacity coefficient unitless
+lnphiv(t,p,tc,pc,w) natural log vapor fugacity coefficient  unitless
+                                                                     
+The functions listed above have been validated against the data in    
+the tables on pages 6.11 and 6.12 of the 5th edition of The Properties
+of Gases and Liquids [1].                                                 
+
+References
+----------
+.. [1] B. E Poling, J. M. Prausnitz, J. P. O'Connell, The Properties
+   of Gases and Liquids 5th edition,  McGraw-Hill, New York (2001).
+"""
 import numpy as np
 
-rg = 8.31447215 # gas constant in J/mol/K
+# gas constant in J/mol/K
+rg = 8.31447215 
 
 # ------------------------------------------------------------------------ #
 # Auxillary functions to calculate the quantities to place the EOS into    #
 # the cubic z form.                                                        #
 # ------------------------------------------------------------------------ #
-def kappa(w): # kappa parameter for SRK EOS
-    x = 0.480 + 1.574*w - 0.176*w**2
-    return(x) # unitless
 
-def alpha(t,tc,w): # alpha for SRK EOS
-    x = (1.0+kappa(w)*(1.0-(t/tc)**0.5))**2
-    return(x) # unitless
+def kappa(w):
+	r""" kappa parameter for the SRK EOS
+	
+	Parameters
+	----------
+	w : float
+		acentric factor of the compound (unitless)
 
-def a(tc,pc): # a for SRK EOS
-    x = 0.42748*rg**2*tc**2/pc
-    return(x) # Pa*m**6/mol**2
+	Returns
+	-------
+	float
+		kappa = 0.480 + 1.574*w - 0.176*w**2 (unitless) 	
+	
+	""" 
+	x = 0.480 + 1.574*w - 0.176*w**2
+	return(x)
 
-def b(tc,pc): # b for SRK EOS
-    x = 0.08664*rg*tc/pc
-    return(x) # m**3/mol
+def alpha(t,tc,w):
+	r""" alpha function for the SRK EOS
+	
+	Parameters
+	----------
+	t : float
+		system temperature (K)
+	
+	tc : float
+		critical temperature of the compound (K)
+	
+	w : float
+		acentric factor of the compound (unitless)
+
+	Returns
+	-------
+	float
+		alpha = (1.0+kappa(w)*(1.0-(t/tc)**0.5))**2 (unitless)	
+	
+	""" 
+	x = (1.0+kappa(w)*(1.0-(t/tc)**0.5))**2
+	return(x)
+
+def a(tc,pc):
+	r""" a parameter for the SRK EOS
+	
+	Parameters
+	----------
+	tc : float
+		critical temperature of the compound (K)
+	
+	pc : float
+		critical pressure of the compound (Pa)
+
+	Returns
+	-------
+	float
+		a = 0.42748*rg**2*tc**2/pc (Pa*m**6/mol**2)	
+	
+	""" 
+	x = 0.42748*rg**2*tc**2/pc
+	return(x)
+
+def b(tc,pc):
+	r""" b parameter for the SRK EOS
+	
+	Parameters
+	----------
+	tc : float
+		critical temperature of the compound (K)
+	
+	pc : float
+		critical pressure of the compound (Pa)
+
+	Returns
+	-------
+	float
+		b = 0.08664*rg*tc/pc (m**3/mol)	
+	
+	""" 
+	x = 0.08664*rg*tc/pc
+	return(x)
 
 def ThetaPrime(t,p,tc,pc,w): # Theta prime for the cubic form of the SRK EOS
     x = a(tc,pc)*alpha(t,tc,w)*p/(rg*t)**2
